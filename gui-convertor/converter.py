f2c6d3832e374ff537922b83f22ee7492f892233
Alex Franz <aaf@intetics.com>
2023-03-27 16:25:52 +0200

    inch to meters convertor

---
+++ gui-convertor/converter.py
@@ -0,0 +1,2 @@
+def convert_to_meters(feet=0, inches=0):
+  return (feet * 12 + inches) * 0.0254
--- gui-convertor/gui.py
+++ gui-convertor/gui.py
@@ -1,13 +1,36 @@
 import PySimpleGUI as sg
+import converter as c
 
-enter_feet_text = sg.Text("Enter the feet", font=("Helvetica", 15))
-enter_feet_input = sg.InputText(tooltip="Enter the feet", font=("Helvetica", 15))
+enter_feet_text = sg.Text("Enter the feet")
+enter_feet_input = sg.InputText(tooltip="Enter the feet", default_text="0")
 
-enter_inches_text = sg.Text("Enter the inches", font=("Helvetica", 15))
-enter_inches_input = sg.InputText(tooltip="Enter the inches", font=("Helvetica", 15))
+enter_inches_text = sg.Text("Enter the inches")
+enter_inches_input = sg.InputText(tooltip="Enter the inches", default_text="0")
 
-convert_button = sg.Button("Convert", font=("Helvetica", 15))
+result_text = sg.Text("Result")
+result_value = sg.Text("", key="result")
 
-window = sg.Window("GUI Convertor", layout=[[enter_feet_text, enter_feet_input], [enter_inches_text, enter_inches_input], [convert_button]])
-window.read()
+convert_button = sg.Button("Convert")
+
+layout = [
+  [enter_feet_text, enter_feet_input], 
+  [enter_inches_text, enter_inches_input], 
+  [convert_button], 
+  [result_text, result_value]
+]
+
+window = sg.Window("GUI Convertor", layout=layout, font=("Helvetica", 15))
+
+while True:
+  event, values = window.read()
+  print("Event:", event, "Values:", values)
+  match event:
+    case "Convert":
+      feet = int(values[0])
+      inches = int(values[1])
+      result_value = c.convert_to_meters(feet, inches)
+      window["result"].update(value=f"{result_value}")
+    case sg.WIN_CLOSED:
+      break
+
 window.close()
