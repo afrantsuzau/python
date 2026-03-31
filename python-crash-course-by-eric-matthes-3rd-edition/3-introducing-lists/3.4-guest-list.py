f37bdb8ebcca6509d58d0c962f17c41cb0129991
Aliaksandr Frantsuzau <aaf@intetics.com>
2026-03-31 11:51:55 +0200

    Chapter 3 (Introducting Lists): ex 3.7 - Shrinking Guests List

--- python-crash-course-by-eric-matthes-3rd-edition/3-introducing-lists/3.4-guest-list.py
+++ python-crash-course-by-eric-matthes-3rd-edition/3-introducing-lists/3.4-guest-list.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+# 3.5 Changing Guests List
 primary_invitation_list = ['Tom', 'billy', 'James', 'oliver', 'Jimmy', 'nick']
 secondary_invitation_list = ['Kevin', 'Bob', 'Stuart']
 event_name = "Dinner"
@@ -33,6 +34,7 @@ print_invitation_message(primary_invitation_list)
 
 
 # 3.6 More Guests
+# You just found a bigger dinner table, so now more space is available. Think of more quests to invite to dinner.
 table_guests_limit = 12
 print(f"\n\nA bigger table with {table_guests_limit} seats had been found. We can invite more people!")
 
@@ -57,3 +59,22 @@ while len(primary_invitation_list) < table_guests_limit:
   print(f"Adjusted list of invitations: {primary_invitation_list}")
   
 print_invitation_message(primary_invitation_list)
+
+# 3.7 Shrinking Guests List
+# You just found out that your new dinner table won't arrive in time for the dinner, and now you have space for only two guests.
+print(f"\n\n\nUnfortunately we can invite only two people to the {event_name} :( \n")
+
+while len(primary_invitation_list) > 2:
+  rejected_guest = primary_invitation_list.pop()
+  print(f"The invitation for {rejected_guest} has been rejected")
+
+print("Send out the remaining invitation confirmations")
+for guest in primary_invitation_list:
+  print(f"Hey {guest}, you are still invited for the {event_name} at {event_time}") 
+
+print("\nClean up the invitation list")
+print(f"The invitation list before clean up: {primary_invitation_list}")
+for index in range(len(primary_invitation_list)):
+  del primary_invitation_list[index - 1]
+  
+print(f"The invitation list after clean up: {primary_invitation_list}")
